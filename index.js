9ebb61defa49dd7f2ca49cb8e1f119514b28e8ee
Alina Pisarenko <pisarenko.alina@gmail.com>
2023-04-16 13:24:17 -0400

    setup basic UI

---
+++ index.js
@@ -0,0 +1,7 @@
+const input = document.getElementById('input-field')
+const addButton = document.getElementById('add-button')
+
+addButton.addEventListener('click', () => {
+    const inputValue = input.value
+    console.log(inputValue)
+})
