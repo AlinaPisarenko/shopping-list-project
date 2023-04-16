0bd6d4dae5d4cedccabef498a0c50c651391bfa9
Alina Pisarenko <pisarenko.alina@gmail.com>
2023-04-16 13:48:02 -0400

    connected to firebase DB

--- index.js
+++ index.js
@@ -1,7 +1,21 @@
+import { initializeApp } from 'https://www.gstatic.com/firebasejs/9.19.1/firebase-app.js'
+import { getDatabase, ref, push } from 'https://www.gstatic.com/firebasejs/9.19.1/firebase-database.js'
+
+
+const appSettings = {
+    databaseURL: "https://playground-8b971-default-rtdb.firebaseio.com/"
+}
+
+const app = initializeApp(appSettings)
+const database = getDatabase(app)
+const itemsInDB = ref(database, 'items')
+
+console.log()
+
 const input = document.getElementById('input-field')
 const addButton = document.getElementById('add-button')
 
 addButton.addEventListener('click', () => {
     const inputValue = input.value
-    console.log(inputValue)
+    push(itemsInDB, inputValue)
 })
