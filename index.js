c055d730880865281c605f506795355f628102b8
Alina Pisarenko <pisarenko.alina@gmail.com>
2023-04-16 14:52:17 -0400

    added real time DB update functionality

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 import { initializeApp } from 'https://www.gstatic.com/firebasejs/9.19.1/firebase-app.js'
-import { getDatabase, ref, push } from 'https://www.gstatic.com/firebasejs/9.19.1/firebase-database.js'
+import { getDatabase, ref, push, onValue } from 'https://www.gstatic.com/firebasejs/9.19.1/firebase-database.js'
 
 
 const appSettings = {
@@ -14,8 +14,30 @@ console.log()
 
 const input = document.getElementById('input-field')
 const addButton = document.getElementById('add-button')
+const list = document.getElementById('shopping-list')
+
+onValue(itemsInDB, (snapshot) => {
+    clearList()
+    let itemsArray = Object.values(snapshot.val())
+    itemsArray.forEach(item => appendItemToList(item))
+})
+
+const appendItemToList = (item) => {
+    list.innerHTML += `<li>${item}</li>`
+}
+
+const clearList = () => {
+    list.innerHTML = ''
+}
+
+const resetInput = () => {
+    input.value = ''
+}
+
+// EVENT LISTENERS
 
 addButton.addEventListener('click', () => {
     const inputValue = input.value
     push(itemsInDB, inputValue)
+    resetInput()
 })
